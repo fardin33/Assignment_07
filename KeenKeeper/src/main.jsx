3b9f3f88a8020179664544fc24530b44aacfafe0
Fardin Hassan <fardin.cse05@gmail.com>
2026-04-14 00:29:12 +0600

    KeenKeeper,Project Setup Done

---
+++ KeenKeeper/src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <h2 className="text-5xl">Hello Fardin</h2>
+  </StrictMode>,
+);
